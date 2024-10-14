1b21664bf68b4a00e811b1971be2350608ec7a66
Benedek Schlesser <schlesser.benedek@petrik.hu>
2024-10-14 13:34:57 +0200

    keszenvagyon

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,69 @@
 import { Controller, Get, Render, Redirect, Req, Res, Body, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 import { Adatok } from './Adatok.dto';
-import { Response, Request } from 'express';
+import { Response} from 'express';
+import { error } from 'console';
 
 
 @Controller()
 export class AppController {
-  #data:Adatok[]=[];
+  #Adatok:Adatok[]=[];
 
   constructor(private readonly appService: AppService) {}
 
-  @Get('foglalas')
+  @Get("")
   @Render('foglalas')
-  foglalas(){
+  getrHello(){
     return{
       data:{},
       errors:[]
     }
   }
 
-  @Post('foglalas')
+  @Post('megadas')
+  megadas(
+    @Body() Adatok:Adatok,
+    @Res() response:Response){
+    
+    console.log(Adatok);
+    let errors=[];
+    if(!Adatok.nev||!Adatok.email||!Adatok.datum||!Adatok.idopont||!Adatok.nezok)
+    {
+      errors.push("Minden mezőt ki kell tölteni!");
+    }
+    if(!/^[^\s@]+@[^\s@]+$/.test(Adatok.email))
+    {
+      errors.push("Nem megfelelő email formátum!");
+    }
+    if(errors.length>0)
+    {
+      response.render("foglalas", {
+        data:Adatok,
+        errors
+      });
+      return
+    }
+    const Megadas:Adatok={
+      nev:Adatok.nev,
+      email:Adatok.email,
+      datum:Adatok.datum,
+      idopont:Adatok.idopont,
+      nezok:Adatok.nezok
+    }
+    this.#Adatok.push(Megadas);
+    console.log(this.#Adatok);
+    return response.redirect("/success");
+  }
 
+
+  @Get("success")
+    @Render("success")
+    rsuccess(){
+      return;
+  }
+
+  @Post("vissza")
+  vissza(@Res() response: Response){
+    return response.redirect('/');
+  }
 }
